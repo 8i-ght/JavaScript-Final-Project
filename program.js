3b88f71bd767d57f3c0772883b8e7a28975ce87f
8i-ght <needboosts3545@gmail.com>
2023-05-08 08:36:11 -0500

    finish insurance button

--- program.js
+++ program.js
@@ -4,9 +4,13 @@ window.addEventListener("load", function(){
     calcCart();
     var cart= document.forms.cart;
     cart.elements.modelQty.onchange = calcCart;
+    var InsuranceOptions = document.querySelectorAll('option[name = "insurance"]');
     var ShippingOptions = document.querySelectorAll('input[name = "shipping"]');
     for (var i = 0; i < ShippingOptions.length; i++) {
       ShippingOptions[i].onclick = calcCart;
+    }
+    for (var i= 0; i < InsuranceOptions.length; i++) {
+      InsuranceOptions[i].onclick = calcCart;
     }
   });
   
@@ -20,9 +24,10 @@ window.addEventListener("load", function(){
       var orderCost = machineCost * qty;
       cart.elements.orderCost.value = formatUSCurrency(orderCost);
   
-      var shipCost = document.querySelector('input[name = "shipping"]:checked').value * qty;
+      var shipCost = document.querySelector('input[name = "shipping"]:checked').value * 1;
       cart.elements.shippingCost.value = formatNumber(shipCost, 2);
   
+      
       cart.elements.subTotal.value = formatNumber(orderCost + shipCost, 2);
   
   
@@ -32,7 +37,6 @@ window.addEventListener("load", function(){
       cart.elements.cartTotal.value = formatUSCurrency((orderCost+shipCost+salesTax), 2);
   
       cart.elements.shippingType.value = document.querySelector('input[name ="shipping"]:checked').nextSibling.nodeValue;
-              
       }
   
   
